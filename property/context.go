92093bd54078171d37e25f7eae43739eb62425fa
james.nesbitt@wunder.io <james.nesbitt@wunder.io>
2017-03-30 12:20:44 +0300

    added context property for operation execution timepouts

--- property/context.go
+++ property/context.go
@@ -1,6 +1,10 @@
 package property
 
-import "context"
+import (
+	"context"
+	api "github.com/CoachApplication/api"
+	base "github.com/CoachApplication/base"
+)
 
 // StringProperty Base Property for Properties that hold a single string
 type ContextProperty struct {
@@ -17,6 +21,15 @@ func (cp *ContextProperty) Type() string {
 	return "context.Context"
 }
 
+// Type string label for content type of property value
+func (cp *ContextProperty) Validate() api.Result {
+	if cp.val == nil {
+		return base.MakeFailedResult()
+	} else {
+		return base.MakeSuccessfulResult()
+	}
+}
+
 // Get retrieve a value from the Property
 func (cp *ContextProperty) Get() interface{} {
 	return interface{}(cp.val)
@@ -30,4 +43,39 @@ func (cp *ContextProperty) Set(val interface{}) error {
 	} else {
 		return error(PropertyValWrongType{Id: cp.Id(), Type: cp.Type(), Val: val})
 	}
+}
+
+const (
+	PROPERTY_ID_CONTEXTLIMIT = "run.context"
+)
+
+type ContextLimitProperty struct {
+	ContextProperty
+}
+
+func NewContextLimitProperty(ctx context.Context) *ContextLimitProperty {
+	p := &ContextLimitProperty{}
+	p.Set(ctx)
+	return p
+}
+
+func (clp *ContextLimitProperty) Property() api.Property {
+	return api.Property(clp)
+}
+
+func (clp *ContextLimitProperty) Id() string {
+	return PROPERTY_ID_CONTEXTLIMIT
+}
+
+func (clp *ContextLimitProperty) Ui() api.Ui {
+	return base.NewUi(
+		clp.Id(),
+		"Limit context",
+		"Limit operation runtime based on a context",
+		"",
+	)
+}
+
+func (clp *ContextLimitProperty) Usage() api.Usage {
+	return base.OptionalPropertyUsage{}.Usage()
 }
